2706ec8fd9e25d7ac8104e6d8230756d95b3a334
emreedincik5 <emreedincik5@gmail.com>
2026-01-06 15:44:48 +0300

    taskitemscreen update

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +17,14 @@ fun TaskItemScreen() {
 
     // TODO 1: Create a mutable state to hold completion status (Boolean)
     // Initial value should be false
+    var isCompleted by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Submit Final Project",
+            style = MaterialTheme.typography.titleLarge
+        )
+
 
     Column(modifier = Modifier.padding(16.dp)) {
 
@@ -25,19 +35,16 @@ fun TaskItemScreen() {
 
         // TODO 2: Show text "Completed" or "Not Completed"
         // depending on completion state
+        Text(text = if (isCompleted) "Completed" else "Not Completed")
 
         Button(
             onClick = {
                 // TODO 3: Toggle completion state
+                isCompleted = !isCompleted
             }
         ) {
             Text("Change Status")
         }
     }
-}
+    }}
 
-@Preview(showBackground = true)
-@Composable
-fun TaskItemPreview() {
-    // TODO: Call TaskItemScreen
-}
